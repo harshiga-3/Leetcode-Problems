df043d1489d2e9b8c387f9c70e62fb85601a4d82
K Harshiga <155134463+harshiga-3@users.noreply.github.com>
2026-01-05 18:56:30 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (40.94%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+      Set <Integer>s=new HashSet<>();
+      for(int n:nums)
+      {
+        if(s.contains(n))
+        {
+            return n;
+        }
+        s.add(n);
+      }
+
+      return -1;
+
+    }
+}
