28e42bf09fe5410b219a8de98e8349ce026f303b
K Harshiga <155134463+harshiga-3@users.noreply.github.com>
2026-03-08 21:05:50 +0530

    Time: 3 ms (62.87%) | Memory: 46.5 MB (93.19%) - LeetSync

---
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+      int m=mat.length;
+      int [][]arr=new int[m][2];
+
+      for(int i=0;i<m;i++)
+{
+    int count=0;
+    for(int j=0;j<mat[0].length;j++)
+    {
+        if(mat[i][j]==1)
+        {
+            count++;
+        }
+    }
+
+    arr[i][0]=count;
+    arr[i][1]=i;
+}
+
+
+Arrays.sort(arr,(a,b)->{
+if(a[0]==b[0])
+
+    return a[1]-b[1];
+return a[0]-b[0];
+
+});
+
+ int[] res=new int[k];
+
+        for(int i=0;i<k;i++)
+            res[i]=arr[i][1];
+
+        return res;
+
+    }
+}
